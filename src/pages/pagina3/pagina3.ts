05c82063010ad8044cf468b849c602bc5bf4bfb4
Pedro Luiz da Silva Amorim <pamorim.dev@gmail.com>
2017-05-24 14:23:26 -0300

    commit inicial com icon e splash

---
+++ src/pages/pagina3/pagina3.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-pagina3',
+  templateUrl: 'pagina3.html'
+})
+export class Pagina3Page {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
